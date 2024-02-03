838a1fdb0af9a74d0d1a115733ae10476f3a6ba8
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-03 14:10:44 +0000

    Install react-router-dom@5.3.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <h1>Home</h1>
+        <h1>Sign In</h1>
+      </Container>
     </div>
   );
 }
