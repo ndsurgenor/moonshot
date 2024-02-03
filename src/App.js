3348bfbcaba8150c71a978173047ece904489a0a
Nathan Surgenor <ndsurgenor@gmail.com>
2024-02-03 15:40:45 +0000

    Add routes and Style navbar links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <Container>
-        <h1>Home</h1>
-        <h1>Sign In</h1>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+        </Switch>
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 import { ImEarth } from "react-icons/im";
 import { MdOutlineHub } from "react-icons/md";
@@ -10,15 +12,23 @@ import { FaArrowRightToBracket } from "react-icons/fa6";
 
 const NavBar = () => {
     return (
-        <Navbar className="bg-body-tertiary" expand="md" ficed="top" data-bs-theme="dark">
+        <Navbar className={styles.NavBar} expand="md" fixed="top" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand>moonshot</Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand className={styles.NavBrand}>moonshot</Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link><ImEarth /> Home</Nav.Link>
-                        <Nav.Link><MdOutlineHub /> Sign Up</Nav.Link>
-                        <Nav.Link><FaArrowRightToBracket /> Sign In</Nav.Link>
+                        <NavLink className={styles.NavLink} activeClassName={styles.ActiveLink} exact to="/">
+                            <ImEarth />Home
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.ActiveLink} to="/signup">
+                            <MdOutlineHub />Sign Up
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.ActiveLink} to="/signin">
+                            <FaArrowRightToBracket />Sign In
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse >
             </Container >
